f676d583431ff1bd1318ca64b9d26422c943000d
liamby12 <72972696+liamby12@users.noreply.github.com>
2020-12-16 13:42:06 +0000

    Experimenting with data returned from the Github API

--- GitHubAPI.py
+++ GitHubAPI.py
@@ -11,3 +11,14 @@ user = g.get_user(username)
 
 for repo in user.get_repos():
     print(repo)
+
+import requests
+
+# github username
+username = "phadej"
+# url to request
+url = f"https://api.github.com/users/{username}"
+# make the request and return the json
+user_data = requests.get(url).json()
+# pretty print JSON data
+pprint(user_data)
